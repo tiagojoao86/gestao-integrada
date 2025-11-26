962d61270a72a08b7e79fd265a92df3ac0b5e4e6
Tiago J Pereira <pereira.tiagojoao@gmail.com>
2025-11-26 17:03:03 -0300

    feat(loading): add loading component, service and interceptor; register in AppComponent

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './components/base/loading/loading-interceptor';
+import { LoadingComponent } from './components/base/loading/loading.component';
 
 @Component({
     selector: 'gi-app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, LoadingComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    providers: [
+      { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    ]
 })
 export class AppComponent {
   title = 'frontend';
